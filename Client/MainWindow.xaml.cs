83586e3a0ba8211653ea563c320eb64a07fbb18f
LionTion <liontionhd@gmail.com>
2025-01-05 23:06:45 +0100

    Added Login Part of Client

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Resources;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +23,46 @@ namespace Client
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            connect();
+        }
+
+        private void Textbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                connect();
+            }
+        }
+
+        private void connect()
+        {
+            string username = usernameTextbox.Text.Trim();
+            if (username.Length < 1)
+            {
+                MessageBox.Show(Utils.GetErrorMessage(this, "noUsername"), "DotNetChat", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            string ip = ipTextbox.Text.Trim();
+            var (code, res) = Utils.ParseIP(ip);
+            switch (code)
+            {
+                case 0:
+                    MessageBox.Show("Connecting to " + res.Address.ToString() + ":" + res.Port + "! (If this worked)" , "DotNetChat", MessageBoxButton.OK, MessageBoxImage.Information);
+                    break;
+                case 1:
+                    MessageBox.Show(Utils.GetErrorMessage(this, "ipParseError"), "DotNetChat", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case 2:
+                    MessageBox.Show(Utils.GetErrorMessage(this, "invalidHost"), "DotNetChat", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+                case 3:
+                    MessageBox.Show(String.Format(Utils.GetErrorMessage(this, "invalidPortNumber"), ip.Split(":")[1]), "DotNetChat", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
         }
     }
 }
---
+++ Client/Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client
+{
+    class Utils
+    {
+        /// <summary>
+        /// Nimmt aus dem aktuellen Fenster-Kontext (Culture) die Übersetzung für eine Error Nachricht heraus.
+        /// <br></br>
+        /// Gibt <b>null</b> zurück bei keinem Treffer.
+        /// </summary>
+        /// <param name="w">Fenster-Kontext</param>
+        /// <param name="key">Schlüssel der Error Nachricht</param>
+        /// <returns>Die übersetze Error Nachricht</returns>
+        public static string? GetErrorMessage(Window w, string key)
+        {
+            try
+            {
+                return (string)((ResourceDictionary)w.FindResource("errors"))[key];
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public static (int, IPEndPoint?) ParseIP(string ip)
+        {
+            if (ip.Length < 1)
+            {
+                return (1, null);
+            }
+            IPEndPoint endpoint = new IPEndPoint(0, 0);
+            if (IPEndPoint.TryParse(ip, out endpoint))
+            {
+                return (0, endpoint);
+            }
+            string[] tokens = ip.Split(':');
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry(tokens[0]);
+                if (host.AddressList.Length < 1)
+                {
+                    return (2, null);
+                }
+                if (tokens.Length > 1)
+                {
+                    short port;
+                    if (short.TryParse(tokens[1], out port) && port > 0)
+                    {
+                        return (0, new IPEndPoint(host.AddressList[0], port));
+                    }
+                    return (3, null);
+                }
+                return (0, new IPEndPoint(host.AddressList[0], 5063));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return (1, null);
+        }
+    }
+}
