8caadcc0114cb89fa3bde21b77e15ff2bb59ae1c
LionTion <liontionhd@gmail.com>
2025-01-05 23:40:32 +0100

    Created Client Globals

---
+++ Client/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Global
+    {
+        public static string username;
+        public static IPEndPoint IPEndPoint;
+    }
+}
